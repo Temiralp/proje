d3bad0b5dc13c794dbafe3a96444268a87e12bb1
test <temiralpin@gmail.com>
2023-07-24 21:31:13 +0300

    main3 fonksiyonu eklendi

--- proje.cs
+++ proje.cs
@@ -63,4 +63,17 @@ class Program
         PrintTeacherInfo();
         PrintStudentInfo();
     }
+    static void Main3()
+    {
+        // Öğretmen ve öğrenci bilgilerini atıyoruz
+        teacher1Name = "Ahmet";
+        teacher1Subject = "Matematik";
+        student1Name = "Ayşe";
+        student1Major = "Bilgisayar Mühendisliği";
+        student1Advisor = teacher1Name;  // Danışman olarak öğretmeni atıyoruz
+
+        // Bilgileri yazdırıyoruz
+        PrintTeacherInfo();
+        PrintStudentInfo();
+    }
 }
